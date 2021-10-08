d3895ee280abc135bafd31c2e7b05a86fc2036c8
hengzhengping <hengzhengping@gmail.com>
2021-10-08 21:10:02 +0800

    Start Dashboard activity after Login

--- SoCyc/app/src/main/java/com/dubailizards/so_cyc/ui/login/Login.java
+++ SoCyc/app/src/main/java/com/dubailizards/so_cyc/ui/login/Login.java
@@ -34,7 +34,7 @@ public class Login extends AppCompatActivity {
 
         FirebaseUser user = mAuth.getCurrentUser();
         if(user!=null){
-            Intent intent = new Intent(getApplicationContext(), Profile.class);
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(intent);
         }
     }
@@ -102,7 +102,7 @@ public class Login extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             FirebaseUser user = mAuth.getCurrentUser();
-                            Intent intent = new Intent(getApplicationContext(), Profile.class);
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                             startActivity(intent);
                         } else {
                             // If sign in fails, display a message to the user.
