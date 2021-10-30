420d21c5cfe7095cfb9ee93e8ee3184d68bd74f0
zhitern <tangzhitern@gmail.com>
2021-10-30 23:25:26 +0800

    added public modifier for TaskLoadedCallback

--- SoCyc/app/src/main/java/com/dubailizards/so_cyc/boundary/navigation/directionhelpers/TaskLoadedCallback.java
+++ SoCyc/app/src/main/java/com/dubailizards/so_cyc/boundary/navigation/directionhelpers/TaskLoadedCallback.java
@@ -1,5 +1,5 @@
 package com.dubailizards.so_cyc.boundary.navigation.directionhelpers;
 
 public interface TaskLoadedCallback {
-    void onTaskDone(Object... values);
+    public void onTaskDone(Object... values);
 }
